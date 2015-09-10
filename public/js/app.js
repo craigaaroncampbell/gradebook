2c13748d803c0255af78f01aa20e64068cf9a554
Craig Campbell <craig.campbell8@gmail.com>
2015-09-09 23:04:10 -0700

    dynamic id working for assignment headers, but needs logic to exclude multiple elements with same id

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@
 
 console.log('app.js file is being read');
 
+var assignmentList = [];
+
 function newStudent(){
   this.studentName = document.getElementById('studentName').value;
   this.letterGrade = 'need method still...'
@@ -11,29 +13,46 @@ function newStudent(){
   var newStudentName = document.createElement('tr');
   table.appendChild(newStudentName);
   newStudentName.innerHTML = '<td class="editable">' + this.studentName + '</td> <td class="letterGrade">' + this.letterGrade + '</td> <td class="percentGrade">' + this.percentGrade + '</td>';
+  addAllAssignments();  // make columns for all assignments created so far for the new student
 }
 
 var addStudent = document.getElementById('addStudent');
 addStudent.addEventListener('click', newStudent, false);
 
-
-function newAssignment(score){
-  this.score = score;
-  var assignmentName = document.getElementById('assignmentName').value;
-  var newAssignmentName = document.createElement('td');
-  $('#gradePercent').after('<th class="editable" id=' + assignmentName +'>' + assignmentName + '</th>');
-  $('.percentGrade').after('<td class="editable"></td>');
+//do someting with this.score. not used yet...
+function newAssignment(assignmentName){
+  // var assignmentName = nameOfAssignment;
+  if (assignmentList.indexOf(assignmentName)){ //if the assignment is NOT already in the list then....
+    $('#gradePercent').after('<th class="editable" id=' + assignmentName +'>' + assignmentName + '</th>'); //put put new table header  with assignemnt name after the  grade percent column (newest assignment to the left, older ones pushed to the right)
+    $('.percentGrade').after('<td class="editable"></td>'); //new table data cells for EVERY student row
+    assignmentList.unshift(assignmentName); //put new assignment name in the front of assignmentList array  (reverse order because the assignment cells will show most recent to the left and oldest to the right)
+  }
+  console.log(assignmentList)
 }
 
 var addAssignment = document.getElementById('addAssignment');
-addAssignment.addEventListener('click', newAssignment, false);
+addAssignment.addEventListener('click', function() {
+  newAssignment(document.getElementById('assignmentName').value)
+}, false);
+
+
+function addAllAssignments(){
+  assignmentList.forEach(function(current, index, array){
+    newAssignment(assignmentList[index])
+  });
+}
 
 function editCells(){
   $('.clicked').text(''); // clear the old text
-  $('<input>').attr({ type: 'text', id: 'editing', name: 'editing'}).appendTo('.clicked'); // insert a text input box
-  $('#editing').focus();
+  $('<input>').attr({ type: 'text', id: 'editing'}).appendTo('.clicked'); // insert a text input box
+  $('#editing').focus(); //put focus on the new text input box
+   console.log( $('#editing').val())
   $('#editing').on('blur', function(){
-    $('.clicked').text($('#editing').val()); //set new table cell text
+    var editValue =  $('#editing').val()
+    $('.clicked').text(editValue); //set new table cell text
+    if ($('.clicked').is($('th'))) {   // if cell is a th element
+      $('.clicked').attr('id', editValue); // set id to the edited value
+    }
     $('.clicked').attr('class', 'editable'); //make the table cell editable again
     $('#editing').remove(); //remove the text input box
   });
@@ -42,5 +61,12 @@ function editCells(){
 $('table').on('click', '.editable',  function(){
   $(this).attr('class', 'clicked');
   editCells();
-});
+}); // if the cell has class 'editable', then change the class to 'clicked' and edit the cell
 
+function deleteStudent(){
+
+}
+
+function deleteAssignment(){
+
+}
