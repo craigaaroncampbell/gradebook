d5510d2aff61d10bcc8c5f9f011688d5b2841d4c
Craig Campbell <craig.campbell8@gmail.com>
2015-09-22 14:05:53 -0700

    getContentType() is now passed the request object

--- gradebook-server.js
+++ gradebook-server.js
@@ -6,7 +6,8 @@ var path = require('path');
 var port = 5000;
 var hostUrl = '127.0.0.1';
 
-function getContentType(extname){
+function getContentType(req){ // should just take a req object as parameter, do the extraction of hte extname inside this function
+  var extname = path.extname(req.url);
   if (extname === '.html'){
     return 'text/html';
   }
@@ -30,7 +31,7 @@ var svr = http.createServer(function(req, res){
       res.writeHead(404);
       res.end("404 - we ain't finding no such file!");
     }
-    res.writeHead(200, {'Content-Type': getContentType(path.extname(req.url))})
+    res.writeHead(200, {'Content-Type': getContentType(req)})
     res.end(data, 'utf-8');
   })
 }).listen(port, hostUrl, function() {
